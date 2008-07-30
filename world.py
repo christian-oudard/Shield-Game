714e5eb0815c03ee018df5a98de9bc39d67b4bf0
gorgapor <gorgapor@35bedb40-ae74-4ce4-95d1-460f42495af1>
2008-07-30 21:47:36 +0000

    Put movement functions in the entity class.
    git-svn-id: file:///home/christian/devel/old_stuff/repos/curses_game@2 35bedb40-ae74-4ce4-95d1-460f42495af1

--- world.py
+++ world.py
@@ -4,27 +4,38 @@ import log
 import vec
 
 class Entity(object):
+    def __init__(self, world, pos):
+        self.world = world
+        self.pos = pos
+
+    def move(self, dir_vec):
+        self.pos = vec.add(self.pos, dir_vec)
+        if self.world.check_bounds(self.pos):
+            return False
+        return True
+
+class Hero(Entity):
     pass
 
-class Box(object):
+class Box(Entity):
     pass
 
 class World(object):
     def __init__(self, board_size):
         self.board_size = board_size
-        self.hero = Entity()
-        self.hero.pos = (0, 0)
-        self.boxes = [Box()]
+        self.hero = Hero(self, (0, 0))
+        self.boxes = [Box(self, (3,3))]
 
     def update(self, command):
-        move = command #STUB, all commands are movement commands
-        old_state = deepcopy(self)
-        hero = self.hero
-        hero.pos = vec.add(hero.pos, move)
-        x, y = hero.pos
-        if self.check_bounds(hero.pos):
-            hero.pos = old_state.hero.pos
+        move_dir = command #STUB, all commands are movement commands
+        self.old_state = deepcopy(self)
+        if not self.hero.move(move_dir):
+            self.rollback()
+            return
     
+    def rollback(self):
+        self.hero.pos = self.old_state.hero.pos
+
     def check_bounds(self, pos):
         x, y = pos
         board_x, board_y = self.board_size
